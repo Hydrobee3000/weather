d20c4d8bbed06cac512faff23cf6dd52dbdce902
hydrobee3000 <hydrombee@gmail.com>
2021-11-12 17:07:11 +0600

    display of weather parameters

--- src/components/DayWeatherContainer/DayWeather/DayWeather.jsx
+++ src/components/DayWeatherContainer/DayWeather/DayWeather.jsx
@@ -25,12 +25,13 @@ const DayWeather = ({ weatherData }) => {
         {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
         {new Date().getFullYear()}
       </p>
-      {/* <p>Текущая температура: {(weatherData.main.temp - 273.15).toFixed(1)} °С</p>
-        <p>Ошушается как: {(weatherData.main.feels_like - 273.15).toFixed(1)} °С</p>
-        <p>Min t: {(weatherData.main.temp_min - 273.15).toFixed(1)} °С</p>
-        <p>Max t: {(weatherData.main.temp_max - 273.15).toFixed(1)} °С</p>
-        <p>Ветер: {weatherData.wind.speed} m/s</p>
-        <p>Влажность: {weatherData.main.humidity}%</p> */}
+      <p>Temperature: {(weatherData.main.temp - 273.15).toFixed(1)} °С</p>
+      <p>Feels like: {(weatherData.main.feels_like - 273.15).toFixed(1)} °С</p>
+      <p>Min t: {(weatherData.main.temp_min - 273.15).toFixed(1)} °С</p>
+      <p>Max t: {(weatherData.main.temp_max - 273.15).toFixed(1)} °С</p>
+      <p>Wind: {weatherData.wind.speed} m/s</p>
+      <p>Humidity: {weatherData.main.humidity}%</p>
+      <p>Pressure: {weatherData.main.pressure}</p>
       {/* <div className='daily-weather'><WeekWeather city={props.city} /></div> */}
     </div>
   )
