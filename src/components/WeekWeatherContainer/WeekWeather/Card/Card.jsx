8e57a9662d6941c4d98c5ade96bc9763fb0e7cc4
hydrobee3000 <hydrombee@gmail.com>
2021-11-13 18:25:33 +0600

    rewrite card component from class to func

--- src/components/WeekWeatherContainer/WeekWeather/Card/Card.jsx
+++ src/components/WeekWeatherContainer/WeekWeather/Card/Card.jsx
@@ -1,20 +1,16 @@
-import { Component } from 'react'
+const Card = ({ day }) => {
+  const ms = day.dt * 1000
+  const weekdayName = new Date(ms).toLocaleString('en', { weekday: 'long' })
 
-class Card extends Component {
-  render() {
-    const ms = this.props.day.dt * 1000
-    const weekdayName = new Date(ms).toLocaleString('en', { weekday: 'long' })
-
-    return (
-      <div>
-        <div className='card-item'>
-          <h2>{weekdayName}</h2>
-          <p>{Math.round(this.props.day.main.temp)} °C</p>
-          <div>{this.props.day.weather[0].description}</div>
-        </div>
+  return (
+    <div>
+      <div className='card-item'>
+        <h2>{weekdayName}</h2>
+        <p>{Math.round(day.main.temp)} °C</p>
+        <div>{day.weather[0].description}</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Card
