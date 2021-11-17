0675e1b87b90a00591c06349d59e705ef19edc09
hydrobee3000 <hydrombee@gmail.com>
2021-11-17 20:52:30 +0600

    changed dayWeather component from JS to TS

--- src/components/DayWeatherContainer/DayWeather/DayWeather.tsx
+++ src/components/DayWeatherContainer/DayWeather/DayWeather.tsx
@@ -2,16 +2,26 @@ import { Container, Typography } from '@mui/material'
 import { useSelector } from 'react-redux'
 import s from './DayWeather.module.css'
 
-const DayWeather = ({ weatherData }) => {
-  const weather = useSelector((state) => state.weather.dayWeatherData.weather[0])
+type weatherDataType = {
+  weatherData: {
+    name: string
+    main: {
+      temp: number
+      feels_like: number
+    }
+    description: string
+  }
+}
 
+const DayWeather = ({ weatherData }: weatherDataType) => {
+  const weather: any = useSelector((state: any) => state.weather.dayWeatherData.weather[0])
   return (
     <Container className={s.wrapper}>
       <Typography style={{ fontSize: '2.5em' }} className={s.title} variant='h2' gutterBottom component='div'>
-        {weather.main} in {weatherData.name}
+        {weather.main} in {weatherData.name} {/* title */}
       </Typography>
-      <img className={s.image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={weatherData.description} />
-
+      <img className={s.image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={weatherData.description} />{' '}
+      {/* weather icon */}
       <Typography style={{ fontSize: '1.2em' }} className={s.parameter} variant='h5' gutterBottom component='div'>
         Temperature: {(weatherData.main.temp - 273.15).toFixed(1)} °С
       </Typography>
@@ -19,13 +29,14 @@ const DayWeather = ({ weatherData }) => {
         Feels like: {(weatherData.main.feels_like - 273.15).toFixed(1)} °С
       </Typography>
       {/* дополнительные параметры, которые, как мне кажется, рядовому пользователю не нужны) *}
+      
       {/* <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
         Min t: {(weatherData.main.temp_min - 273.15).toFixed(1)} °С
       </Typography>
       <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
         Max t: {(weatherData.main.temp_max - 273.15).toFixed(1)} °С
-      </Typography> */}
-      {/* <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
+      </Typography> 
+       <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
         Wind: {weatherData.wind.speed} m/s
       </Typography>
       <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
@@ -33,11 +44,9 @@ const DayWeather = ({ weatherData }) => {
       </Typography>
       <Typography className={s.parameter} variant='h5' gutterBottom component='div'>
         Pressure: {weatherData.main.pressure}
-      </Typography> */}
-
-      {/* <div className='daily-weather'><WeekWeather city={props.city} /></div> */}
-
-      {/* <Typography className={s.date} variant='h4' gutterBottom component='div'>
+      </Typography> 
+     <div className='daily-weather'><WeekWeather city={props.city} /></div> 
+      <Typography className={s.date} variant='h4' gutterBottom component='div'>
         {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
         {new Date().getFullYear()}
       </Typography> */}
