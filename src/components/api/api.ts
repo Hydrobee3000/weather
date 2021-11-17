55417ac5f38d407b8d241e71b410b2ff52f8c84a
hydrobee3000 <hydrombee@gmail.com>
2021-11-17 21:34:02 +0600

    changed api file from JS to TS

---
+++ src/components/api/api.ts
@@ -0,0 +1,16 @@
+// import * as axios from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+const config: AxiosRequestConfig = {
+  baseURL: 'https://api.openweathermap.org/data/2.5/',
+}
+const instance: AxiosInstance = axios.create(config)
+
+export const weatherAPI = {
+  getDayWeather(city: string): any {
+    return instance.get(`weather?q=${city}&appid=5712b8887160185aaa20b84fcd1da1c4`)
+  },
+  getWeekWeather(city: string): any {
+    return instance.get(`forecast?q=${city}&lang=ru&units=metric&APPID=5712b8887160185aaa20b84fcd1da1c4`)
+  },
+}
