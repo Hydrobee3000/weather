2e55f02b885118ae9af44cd7d8013a82d01ba39c
hydrobee3000 <hydrombee@gmail.com>
2021-11-18 17:16:31 +0600

    changed WeekWeatherContainer from JS to TS

--- src/components/WeekWeatherContainer/WeekWeatherContainer.tsx
+++ src/components/WeekWeatherContainer/WeekWeatherContainer.tsx
@@ -4,9 +4,30 @@ import { fetchWeekWeatherData } from './../../redux/reducers/weatherReducer'
 import Preloader from './../common/Preloader/Preloader'
 import WeekWeather from './WeekWeather/WeekWeather'
 
-const WeekWeatherContainer = ({ activePlace }) => {
+type PropsType = {
+  activePlace: string
+}
+
+interface RootState {
+  weather: {
+    weekWeatherData: {
+      cod: string
+      message: number
+      cnt: number
+      list: {
+        dt: number
+        main: { temp: number; feels_like?: number | undefined }
+        weather: { main: string; icon?: string | undefined }[]
+        dt_txt: string
+      }[]
+      city: object
+    }
+  }
+}
+
+const WeekWeatherContainer: React.FC<PropsType> = ({ activePlace }) => {
   const dispatch = useDispatch()
-  const weekWeatherData = useSelector((state) => state.weather.weekWeatherData) //get data
+  const weekWeatherData = useSelector((state: RootState) => state.weather.weekWeatherData) //get data
 
   //fetch data
   useEffect(() => {
