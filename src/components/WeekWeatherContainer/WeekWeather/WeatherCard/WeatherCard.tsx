f42e18b26f0c7c74a337aa1a89b76dd723be9e16
Алексей Заикин <hydrombee@gmail.com>
2022-08-16 21:54:42 +0200

    added hover effect for card

--- src/components/WeekWeatherContainer/WeekWeather/WeatherCard/WeatherCard.tsx
+++ src/components/WeekWeatherContainer/WeekWeather/WeatherCard/WeatherCard.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material'
 import Card from '@mui/material/Card'
+import s from './WeatherCard.module.css'
 
 //type for props
 interface IProps {
@@ -18,22 +19,35 @@ interface IProps {
 
 const WeatherCard: React.FC<IProps> = ({ day }) => {
   const ms = day.dt * 1000
+  const dayOfWeek = new Date(ms).toDateString()
+  console.log(dayOfWeek)
   const dayOfWeekName = new Date(ms).toLocaleString('en', { weekday: 'long' })
 
   return (
-    <Card sx={{ maxWidth: 350, marginLeft: 'auto', marginRight: 'auto', marginTop: '2em', paddingBottom: '1em' }}>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Typography style={{ marginTop: '0.3em', fontSize: '2.6em' }} variant='h2' gutterBottom component='div'>
-          {dayOfWeekName}
-          {/* {<img src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={weatherData.description} />}  'icon for every day*/}
-        </Typography>
-        <Typography style={{ marginBottom: '1em', marginTop: '0.2em' }} variant='h5' gutterBottom component='div'>
-          {Math.round(day.main.temp)} °C
-        </Typography>
-        <Typography style={{ fontSize: '1em' }} variant='h5' gutterBottom component='div'>
-          {day.weather[0].main} {/* weather condition descriptions */}
-        </Typography>
-      </div>
+    <Card
+      className={s.card}
+      sx={{
+        maxWidth: 350,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '2em',
+        paddingBottom: '1em',
+        borderRadius: '10px',
+      }}
+    >
+      <Typography style={{ marginTop: '0.3em', fontSize: '2.6em' }} variant='h2' gutterBottom component='div'>
+        {dayOfWeekName}
+        {/* {<img src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={weatherData.description} />}  'icon for every day*/}
+      </Typography>
+      <Typography style={{ marginBottom: '1em', marginTop: '0.2em' }} variant='h5' gutterBottom component='div'>
+        {Math.round(day.main.temp)} °C
+      </Typography>
+      <Typography style={{ fontSize: '1em' }} variant='h5' gutterBottom component='div'>
+        {day.weather[0].main} {/* weather condition descriptions */}
+      </Typography>
+      <Typography style={{ marginTop: '0.5em', opacity: '0.5' }} gutterBottom component='div'>
+        {dayOfWeek}
+      </Typography>
     </Card>
   )
 }
