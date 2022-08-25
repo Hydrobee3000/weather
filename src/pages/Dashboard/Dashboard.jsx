d0e92c3c2187e443119cdc06bd8e0e8fb7f25557
Алексей Заикин <hydrombee@gmail.com>
2022-08-25 20:54:48 +0200

    added card for dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,48 @@
 import { useSelector } from 'react-redux'
+import { Space, Typography } from 'antd'
+import { Card } from 'antd'
+import { flexbox } from '@mui/system'
+import { ExportOutlined } from '@ant-design/icons'
+import s from './Dashboard.module.css'
+
+const { Text } = Typography
 
 const DashboardFC = () => {
   const weather = useSelector((state) => state.weather.dayWeatherData.weather[0]) // get description of weather ex: 'cloudy'
   const dayWeatherData = useSelector((state) => state.weather.dayWeatherData)
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '2em' }}>
-      <p style={{ fontSize: '2em' }}>{dayWeatherData.name}</p>
+    <Space>
+      <Text>Today overview</Text>
 
+      <div className={s.app_wrap} style={{ width: 300, height: 120, display: 'flex' }}>
+        <div
+          className='card__content'
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 10,
+            borderRight: '1px solid black',
+            width: '60px',
+          }}
+        >
+          <ExportOutlined />
+        </div>
+        <Text style={{ paddingLeft: 20, paddingTop: 10, opacity: 0.5, fontSize: '1.2em' }}>Pressure</Text>
+        <div
+          className='card__content'
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ fontSize: '2.5em', fontWeight: '400' }}>16.1 °С</Text>
+        </div>
+      </div>
+
+      <p style={{ fontSize: '2em' }}>{dayWeatherData.name}</p>
       {/* weather icon */}
       {/* <img className={s.image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={dayWeatherData.description} /> */}
       <p>{(dayWeatherData.main.temp - 273.15).toFixed(1)} °С</p>
@@ -16,7 +51,6 @@ const DashboardFC = () => {
         Feels like: {(dayWeatherData.main.feels_like - 273.15).toFixed(1)} °С
       </p> */}
       {/* additional parameters */}
-
       <p>Min t: {(dayWeatherData.main.temp_min - 273.15).toFixed(1)} °С</p>
       <p>Max t: {(dayWeatherData.main.temp_max - 273.15).toFixed(1)} °С</p>
       <p>Wind: {dayWeatherData.wind.speed} m/s</p>
@@ -26,7 +60,7 @@ const DashboardFC = () => {
         {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
         {new Date().getFullYear()}
       </p>
-    </div>
+    </Space>
   )
 }
 
