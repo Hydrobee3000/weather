ca485890463c12270ec55765ed661d759a30b509
Алексей Заикин <hydrombee@gmail.com>
2022-08-27 01:08:01 +0200

    create card for dashboard item

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux'
 import { Space, Typography } from 'antd'
 import { Card } from 'antd'
-import { flexbox } from '@mui/system'
 import { ExportOutlined } from '@ant-design/icons'
 import s from './Dashboard.module.css'
+import { Progress } from 'antd'
+import { WiBarometer } from 'react-icons/wi'
 
 const { Text } = Typography
 
@@ -12,55 +13,45 @@ const DashboardFC = () => {
   const dayWeatherData = useSelector((state) => state.weather.dayWeatherData)
 
   return (
-    <Space>
-      <Text>Today overview</Text>
-
-      <div className={s.app_wrap} style={{ width: 300, height: 120, display: 'flex' }}>
-        <div
-          className='card__content'
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 10,
-            borderRight: '1px solid black',
-            width: '60px',
-          }}
+    <div style={{ paddingLeft: '20px' }}>
+      <Space>
+        <Card
+          headStyle={{ padding: '0 1em' }}
+          bodyStyle={{ paddingBottom: '1.5em' }}
+          style={{ borderRadius: '15px', width: 220, backgroundColor: '#efefef' }}
+          title={<Text style={{ color: '#783fdb' }}>Pressure</Text>}
+          extra={<WiBarometer style={{ fontSize: '2em' }} />}
         >
-          <ExportOutlined />
-        </div>
-        <Text style={{ paddingLeft: 20, paddingTop: 10, opacity: 0.5, fontSize: '1.2em' }}>Pressure</Text>
-        <div
-          className='card__content'
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Text style={{ fontSize: '2.5em', fontWeight: '400' }}>16.1 °С</Text>
-        </div>
-      </div>
+          <Progress
+            // strokeColor='#783fdb'
+            trailColor='#783fdb30'
+            style={{ display: 'flex', justifyContent: 'center' }}
+            type='dashboard'
+            percent={dayWeatherData.main.humidity}
+          />
+        </Card>
+        <Text>Today overview</Text>
 
-      <p style={{ fontSize: '2em' }}>{dayWeatherData.name}</p>
-      {/* weather icon */}
-      {/* <img className={s.image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={dayWeatherData.description} /> */}
-      <p>{(dayWeatherData.main.temp - 273.15).toFixed(1)} °С</p>
-      <p>{weather.main}</p>
-      {/* <p style={{ fontSize: '1.2em' }} className={s.parameter}>
+        <p style={{ fontSize: '2em' }}>{dayWeatherData.name}</p>
+        {/* weather icon */}
+        {/* <img className={s.image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={dayWeatherData.description} /> */}
+        <p>{(dayWeatherData.main.temp - 273.15).toFixed(1)} °С</p>
+        <p>{weather.main}</p>
+        {/* <p style={{ fontSize: '1.2em' }} className={s.parameter}>
         Feels like: {(dayWeatherData.main.feels_like - 273.15).toFixed(1)} °С
       </p> */}
-      {/* additional parameters */}
-      <p>Min t: {(dayWeatherData.main.temp_min - 273.15).toFixed(1)} °С</p>
-      <p>Max t: {(dayWeatherData.main.temp_max - 273.15).toFixed(1)} °С</p>
-      <p>Wind: {dayWeatherData.wind.speed} m/s</p>
-      <p>Humidity: {dayWeatherData.main.humidity}%</p>
-      <p>Pressure: {dayWeatherData.main.pressure}</p>
-      <p>
-        {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
-        {new Date().getFullYear()}
-      </p>
-    </Space>
+        {/* additional parameters */}
+        <p>Min t: {(dayWeatherData.main.temp_min - 273.15).toFixed(1)} °С</p>
+        <p>Max t: {(dayWeatherData.main.temp_max - 273.15).toFixed(1)} °С</p>
+        <p>Wind: {dayWeatherData.wind.speed} m/s</p>
+        <p>Humidity: {dayWeatherData.main.humidity}%</p>
+        <p>Pressure: {dayWeatherData.main.pressure}</p>
+        <p>
+          {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
+          {new Date().getFullYear()}
+        </p>
+      </Space>
+    </div>
   )
 }
 
