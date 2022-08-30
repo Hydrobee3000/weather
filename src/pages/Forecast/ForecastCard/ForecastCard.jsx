c0a94948dbbf66ecccd509c243ffb8e9b9eba0b9
Алексей Заикин <hydrombee@gmail.com>
2022-08-31 00:07:59 +0200

    added weather icons into forecast cards

--- src/pages/Forecast/ForecastCard/ForecastCard.jsx
+++ src/pages/Forecast/ForecastCard/ForecastCard.jsx
@@ -1,6 +1,6 @@
-import { useSelector } from 'react-redux'
-import { Space, Typography, Statistic, Card } from 'antd'
-import { TbWind, TbTemperature, TbCloud, TbGauge } from 'react-icons/tb'
+import { Space, Typography, Card } from 'antd'
+import { IoSunnySharp, IoCloudSharp } from 'react-icons/io5'
+import { BsFillCloudRainHeavyFill } from 'react-icons/bs'
 
 const { Text } = Typography
 
@@ -14,8 +14,20 @@ const ForecastCard = ({ day }) => {
       style={{
         borderRadius: '15px',
         backgroundColor: '#efefef',
+        minWidth: '18em',
       }}
       title={<Text style={{ color: '#783fdb' }}>{dayOfWeekName}</Text>}
+      extra={
+        day.weather[0].main === 'Clear' ? (
+          <IoSunnySharp style={{ fontSize: '1.2em', color: 'white', width: '25px', height: '25px' }} />
+        ) : day.weather[0].main === 'Rain' ? (
+          <BsFillCloudRainHeavyFill style={{ fontSize: '1.2em', color: 'white', width: '25px', height: '25px' }} />
+        ) : day.weather[0].main === 'Clouds' ? (
+          <IoCloudSharp style={{ fontSize: '1.2em', color: 'white', width: '25px', height: '25px' }} />
+        ) : (
+          ''
+        )
+      }
     >
       <Space
         size='large'
@@ -24,7 +36,7 @@ const ForecastCard = ({ day }) => {
       >
         <Text style={{ fontSize: '2em', padding: '0.6em' }}> {Math.round(day.main.temp)} °C</Text>
         <Text style={{ fontSize: '2em', color: '#783fdb' }}>{day.weather[0].main}</Text>
-        <Text style={{ fontSize: '1.6em', padding: '0.7em', opacity: 0.6 }}>{dayOfWeek}</Text>
+        <Text style={{ padding: '0.7em', opacity: 0.6 }}>{dayOfWeek}</Text>
       </Space>
     </Card>
   )
