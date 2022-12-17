051586c705de9a5249f73b40d21c61d4d490f7b3
Алексей Заикин <hydrombee@gmail.com>
2022-12-17 21:35:40 +0100

    dashboardCardsStyle converted to TS

---
+++ src/utils/constants/dashboardCardsStyle.ts
@@ -0,0 +1,8 @@
+// style of header in cards
+export const cardHeadStyle = { padding: '0 1em' } as const
+
+// style of body in cards
+export const cardBodyStyle = { paddingBottom: '1.5em' } as const
+
+// style of value of statistic parameters in statistics cards
+export const statisticValueStyle = { fontSize: '1.85em', textAlign: 'center' } as const
