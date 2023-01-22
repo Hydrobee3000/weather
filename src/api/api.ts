774fc4376e31db624d2d1f702f99d48d6d39cc55
Алексей Заикин <hydrombee@gmail.com>
2023-01-22 20:02:28 +0100

    api converted to TS

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,24 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 
+//  axios config for requests
 const config: AxiosRequestConfig = {
   baseURL: 'https://api.openweathermap.org/data/2.5/',
 }
+
+// create requests axios with config
 const instance: AxiosInstance = axios.create(config)
 
+// weather api
+
 export const weatherAPI = {
+  // get data of weather for the day
+
   getDayWeather(city: string): any {
     return instance.get(`weather?q=${city}&units=metric&appid=5712b8887160185aaa20b84fcd1da1c4`)
   },
+
+  // get data of weather for the week
+
   getWeekWeather(city: string): any {
     return instance.get(`forecast?q=${city}&units=metric&APPID=5712b8887160185aaa20b84fcd1da1c4`)
   },
