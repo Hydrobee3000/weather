25368802d7047bef0dba1fd0ca98d79c90a81852
Алексей Заикин <hydrombee@gmail.com>
2023-01-29 11:15:35 +0100

    Forecast.js converted to TS

--- src/pages/Forecast/Forecast.tsx
+++ src/pages/Forecast/Forecast.tsx
@@ -4,24 +4,26 @@ import ForecastCard from './ForecastCard/ForecastCard' // <FC> of forecast card 
 import { Typography } from 'antd'
 import s from './Forecast.module.css' // css file with styles
 import { pageTitle } from '../../utils/constants/commonStyles'
+import { IRootState } from '../../redux/store'
+import { ForecastData, ForecastDataList } from '../../utils/types'
 
 const { Title } = Typography
 
 // forecast page
 
-const Forecast = () => {
-  const forecastData = useSelector((state) => state.weather.forecastData)
-  const dailyList = forecastData.list.filter((reading) => reading.dt_txt.includes('12:00:00')) // get data of every day per 12:00
-  console.log(forecastData)
+const Forecast: React.FC = () => {
+  const forecastData: ForecastData | null = useSelector((state: IRootState) => state.weather.forecastData) // weather forecast object
+  const dailyList: any = forecastData?.list.filter((reading) => reading.dt_txt.includes('12:00:00')) // get data of every day per 12:00
+  console.log('dailyList: ', dailyList)
 
   // forecast cards with data
   const dailyFormatCards = () => {
-    return dailyList.map((day, index) => <ForecastCard day={day} key={index} />)
+    return dailyList.map((day: any, index: number) => <ForecastCard day={day} key={index} />)
   }
 
   return (
     <div className={s.wrapper}>
-      <Space direction='vertical' size={40}>
+      <Space direction='vertical' size={60}>
         <Title style={pageTitle}>Forecast overview</Title>
 
         {dailyFormatCards()}
