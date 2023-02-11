e431db17391518a02fd93a1491779116160befc6
Алексей Заикин <hydrombee@gmail.com>
2023-02-11 22:39:00 +0100

    CurrentContainer.jsx converted to TS

--- src/pages/Current/CurrentContainer.tsx
+++ src/pages/Current/CurrentContainer.tsx
@@ -1,17 +1,19 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux' // redux hooks
-import Preloader from '../../components/common/Preloader/Preloader' // preloader to wait for data to load
+import Preloader from '../../components/common/Preloader' // preloader to wait for data to load
 import { fetchDayWeatherData } from '../../redux/reducers/weatherReducer' // get weather data with selected location
+import { IRootState } from '../../redux/store'
+import { IDayWeatherData } from '../../utils/types'
 import Current from './Current' // <FC> of current page
 
 // fetch data for current page and set in store
 
-const CurrentContainer = () => {
-  const dispatch = useDispatch()
-  const activePlace = useSelector((state) => state.weather.activePlace) // selected active place
-  const dayWeatherData = useSelector((state) => state.weather.dayWeatherData)
+const CurrentContainer: React.FC = () => {
+  const dispatch: any = useDispatch()
+  const activePlace: string = useSelector((state: IRootState) => state.weather.activePlace) // selected active place
+  const dayWeatherData: IDayWeatherData = useSelector((state: IRootState) => state.weather.dayWeatherData) // weather object
 
-  // fetch data
+  // fetch data with selected place
   useEffect(() => {
     dispatch(fetchDayWeatherData(activePlace))
   }, [dispatch, activePlace])
