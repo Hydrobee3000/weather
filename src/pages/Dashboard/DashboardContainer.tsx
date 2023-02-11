48549593c6b37c1367ce45d3a108431cf275c7bd
Алексей Заикин <hydrombee@gmail.com>
2023-02-11 22:39:23 +0100

    DashboardContainer.jsx converted to TS

---
+++ src/pages/Dashboard/DashboardContainer.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux' // redux hooks
+import Preloader from '../../components/common/Preloader' // preloader to wait for data to load
+import { fetchDayWeatherData } from '../../redux/reducers/weatherReducer' // get weather data with selected location
+import { IRootState } from '../../redux/store'
+import { IDayWeatherData } from '../../utils/types'
+import DashboardFC from './Dashboard' // <FC> of dashboard page
+
+// fetch data for dashboard page and set in store
+
+const DashboardContainer: React.FC = () => {
+  const dispatch: any = useDispatch()
+  const activePlace: string = useSelector((state: IRootState) => state.weather.activePlace) // selected active place
+  const dayWeatherData: IDayWeatherData = useSelector((state: IRootState) => state.weather.dayWeatherData) // weather for the day object
+
+  // fetch data with selected place
+  useEffect(() => {
+    dispatch(fetchDayWeatherData(activePlace))
+  }, [dispatch, activePlace])
+
+  // display preloader, while data is empty
+  if (!dayWeatherData) {
+    return <Preloader />
+  }
+
+  return <DashboardFC />
+}
+
+export default DashboardContainer
