02781512988b07c909888227796088bbf105b0cc
Алексей Заикин <hydrombee@gmail.com>
2023-02-11 22:39:11 +0100

    Dashboard.jsx converted to TS

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,27 +1,28 @@
-import { Space } from 'antd' // antd components
+import { Space, Typography } from 'antd' // antd components
 import { TbWind, TbTemperature, TbCloud, TbDroplet } from 'react-icons/tb' // icons
 import { useSelector } from 'react-redux' // hook for getting value from redux state
 import CardStatistic from './Cards/CardStatistic' // <FC> of card with statistic
 import CardStatisticTemp from './Cards/CardStatisticTemp' // <FC> of card with temperature statistic
 import CardProgress from './Cards/CardProgress' // <FC> of card with progress
 import firstLetterUpperCase from '../../utils/firstLetterUpperCase' // function makes first letter in uppercase style
+import { IRootState } from '../../redux/store'
+import { IDayWeatherData } from '../../utils/types'
 import { pageTitle, primaryColor } from '../../utils/constants/commonStyles' // inline common styles
-import { Typography } from 'antd'
 import s from './Dashboard.module.css' // css file with styles
 
-const { Title } = Typography
+const { Title } = Typography // title antd component
 
 // dashboard page
 
-const DashboardFC = () => {
-  const weather = useSelector((state) => state.weather.dayWeatherData.weather[0]) // get description of weather ex: 'cloudy'
-  const dayWeatherData = useSelector((state) => state.weather.dayWeatherData) // get weather object
+const DashboardFC: React.FC = () => {
+  const weatherDesc: string = useSelector((state: IRootState) => state.weather.dayWeatherData.weather[0].description) // object of weather per day. ex: 'cloudy'
+  const dayWeatherData: IDayWeatherData = useSelector((state: IRootState) => state.weather.dayWeatherData) // weather object
 
   return (
     <>
       <Title style={pageTitle}>
-        <span style={primaryColor}>{firstLetterUpperCase(weather.description)}</span>
-        <span style={{ padding: '0.7em' }}>in</span>
+        <span style={primaryColor}>{firstLetterUpperCase(weatherDesc)}</span>
+        <span style={{ padding: '0 0.7em' }}>in</span>
         <span style={primaryColor}>{dayWeatherData.name}</span>
       </Title>
 
@@ -51,9 +52,9 @@ const DashboardFC = () => {
             fourthData={dayWeatherData.main.temp_min}
           />
 
-          <CardProgress title='Humidity' data={dayWeatherData.main.humidity} icon={<TbDroplet className={s.card__icon} />} />
+          <CardProgress title='Humidity' icon={<TbDroplet className={s.card__icon} />} data={dayWeatherData.main.humidity} />
 
-          <CardProgress title='Cloudiness' data={dayWeatherData.clouds.all} icon={<TbCloud className={s.card__icon} />} />
+          <CardProgress title='Cloudiness' icon={<TbCloud className={s.card__icon} />} data={dayWeatherData.clouds.all} />
 
           {/* <p>Pressure: {dayWeatherData.main.pressure}</p> */}
         </Space>
