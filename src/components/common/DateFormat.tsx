58d2730861773f32ac43f57c8adb425b937d3805
Алексей Заикин <hydrombee@gmail.com>
2023-10-25 23:27:47 +0200

    removed primary color of date in header

--- src/components/common/DateFormat.tsx
+++ src/components/common/DateFormat.tsx
@@ -1,11 +1,9 @@
 import { getFormattedDateShort } from '../../utils/getFormattedDate'
-import { primaryColor } from '../../utils/constants/commonStyles'
 import { Typography } from 'antd'
 import useWindowSize from '../../hooks/useWindowSize'
 import { mobileBreakPointWidth } from '../../utils/constants/mobileBreakPoint'
 
 const { Text } = Typography
-let color = primaryColor.color
 
 // inscription displaying the current date
 
@@ -18,8 +16,8 @@ const FormattedDate = () => {
     <Text
       style={
         currentWidth && currentWidth >= mobileBreakPointWidth
-          ? { margin: '0', marginLeft: '1.5em', fontSize: '1.2em', color: color }
-          : { margin: '0', fontSize: '1em', color: color }
+          ? { margin: '0', marginLeft: '1.5em', fontSize: '1.2em' }
+          : { margin: '0', fontSize: '1em' }
       }
     >
       {currentWidth && currentWidth >= mobileBreakPointWidth ? formattedDate : formattedDate.split(',')[0]}
