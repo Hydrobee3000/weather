25b8d3f49c82db3b1b16027a4d7e5edb59b9b4bc
Алексей Заикин <hydrombee@gmail.com>
2023-11-05 01:41:13 +0100

    added function for getting field value in localStorage

---
+++ src/utils/constants/localStorage.ts
@@ -0,0 +1,35 @@
+export const favoritePlacesKeyLs: string = 'favoritePlaces' // key in localStorage favorite places field
+
+/**
+ * Gets a value from localStorage by key.
+ * @param {string} key - The key to retrieve the value.
+ * @returns {T | null} - Returns the value from localStorage or null if the data is not present.
+ */
+
+export function getFromLocalStorage<T>(key: string): T | null {
+  const data = localStorage.getItem(key)
+  if (data) {
+    return JSON.parse(data) as T
+  }
+  return null
+}
+
+/**
+ * Saves a value to localStorage by key.
+ * @param {string} key - The key to store the value.
+ * @param {T} value - The value to be stored.
+ */
+
+export function saveToLocalStorage<T>(key: string, value: T) {
+  const data = JSON.stringify(value)
+  localStorage.setItem(key, data)
+}
+
+/**
+ * Removes a value from localStorage by key.
+ * @param {string} key - The key to remove the value.
+ */
+
+export function removeFromLocalStorage(key: string) {
+  localStorage.removeItem(key)
+}
