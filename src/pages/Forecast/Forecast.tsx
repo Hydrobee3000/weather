4c178096dd2a537ab269b19047d381cede943ef3
Алексей Заикин <hydrombee@gmail.com>
2023-11-05 05:01:34 +0100

    added icon in title in Forecast  page

--- src/pages/Forecast/Forecast.tsx
+++ src/pages/Forecast/Forecast.tsx
@@ -3,6 +3,7 @@ import { IRootState } from '../../redux/store'
 import { Space } from 'antd' // antd components
 import ForecastCard from './ForecastCard/ForecastCard' // <FC> of forecast card on week
 import PageTitle from '../../components/common/PageTitle/PageTitle'
+import { forecastPageIcons } from '../../utils/constants/pageIcons'
 import { IForecastData, IdailyForecastData } from '../../types/types'
 import s from './Forecast.module.scss' // css file with styles
 
@@ -13,8 +14,8 @@ interface IProps {
 
 const Forecast: React.FC<IProps> = ({ isDarkMode }) => {
   const forecastData: IForecastData = useSelector((state: IRootState) => state.weather.forecastData!) // weather forecast data object
-  // get data of every day per 12:00
-  const dailyList: IdailyForecastData[] = forecastData.list.filter((reading) => reading.dt_txt.includes('12:00:00'))
+  const dailyList: IdailyForecastData[] = forecastData.list.filter((reading) => reading.dt_txt.includes('12:00:00')) // get data of every day per 12:00
+  const IconComponent: React.ElementType = forecastPageIcons.outlined
 
   // forecast cards with data
   const dailyFormatCards = () => {
@@ -25,7 +26,7 @@ const Forecast: React.FC<IProps> = ({ isDarkMode }) => {
 
   return (
     <>
-      <PageTitle>Forecast overview</PageTitle>
+      <PageTitle icon={<IconComponent />}>Forecast overview</PageTitle>
 
       <div className={s.wrapper}>
         <Space direction='vertical' size={60}>
