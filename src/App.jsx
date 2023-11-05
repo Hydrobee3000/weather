18d0061f255c1fd94582cd6aa2fa483a96dcc70f
Алексей Заикин <hydrombee@gmail.com>
2023-11-05 01:47:59 +0100

    added getting favorite places from localStorage on first load

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchDayWeatherData, fetchForecastData } from './redux/reducers/weatherReducer'
+import { fetchDayWeatherData, fetchForecastData, setFavoritePlaces } from './redux/reducers/weatherReducer'
 import { Layout, ConfigProvider, theme } from 'antd'
 import HeaderFC from './components/Header/Header'
 import MenuFC from './components/Menu/Menu'
 import { mobileBreakPointWidth } from './utils/constants/mobileBreakPoint'
 import AppRoutes from './Routes'
 import useWindowSize from './hooks/useWindowSize.ts'
+import { favoritePlacesKeyLs, getFromLocalStorage } from './utils/constants/localStorage'
 
 const { Content } = Layout
 const { defaultAlgorithm, darkAlgorithm } = theme
 
 const App = () => {
   const dispatch = useDispatch()
+  const { currentWidth } = useWindowSize()
   const activePlace = useSelector((state) => state.weather.activePlace) // gets the selected active place
-  const { currentWidth, currentHeight } = useWindowSize()
 
   // theme
   let isPreferDarkTheme = window.matchMedia('(prefers-color-scheme:dark)').matches // determines which theme the user prefers
   const [isDarkMode, setIsDarkMode] = useState(isPreferDarkTheme) // sets the selected default OS theme
+
+  // get favorite places from localStorage
+  useEffect(() => {
+    const favoritePlaces = getFromLocalStorage(favoritePlacesKeyLs)
+    dispatch(setFavoritePlaces(favoritePlaces))
+  }, [])
 
   // fetch data
   useEffect(() => {
