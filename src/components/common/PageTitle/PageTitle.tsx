1727c0c1b26be7dd483503807f7804e219890983
Алексей Заикин <hydrombee@gmail.com>
2023-11-05 02:52:24 +0100

    added common component of page title

---
+++ src/components/common/PageTitle/PageTitle.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react'
+import { Divider, Typography } from 'antd' // antd components
+import s from './PageTitle.module.scss'
+
+const { Title } = Typography
+
+interface IProps {
+  children: ReactNode
+}
+
+// the main title of page
+
+const PageTitle: React.FC<IProps> = ({ children }) => {
+  return (
+    <>
+      <Title className={s.title}>{children}</Title>
+      <Divider className={s.title__divider} />
+    </>
+  )
+}
+
+export default PageTitle
