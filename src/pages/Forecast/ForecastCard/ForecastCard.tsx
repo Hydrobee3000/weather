a3942f76274510cf6eab74dd7bc9df68d5fefb37
Алексей Заикин <hydrombee@gmail.com>
2023-11-05 05:45:41 +0100

    change passing theme mode from props to redux in ForecastCard component

--- src/pages/Forecast/ForecastCard/ForecastCard.tsx
+++ src/pages/Forecast/ForecastCard/ForecastCard.tsx
@@ -1,17 +1,18 @@
+import { useSelector } from 'react-redux'
+import { Space, Typography, Card, Tag } from 'antd'
 import getIcon from '../../../utils/getIcon'
+import { IRootState } from '../../../redux/store'
 import { getFormattedDate, getWeekdayName } from '../../../utils/getFormattedDate'
 import { IdailyForecastData } from '../../../types/types'
-import { Space, Typography, Card, Tag } from 'antd'
 import s from './ForecastCard.module.scss'
 
 const { Text } = Typography
 
 interface IProps {
   dailyData: IdailyForecastData
-  isDarkMode: boolean
 }
 
-const ForecastCard: React.FC<IProps> = ({ dailyData, isDarkMode }) => {
+const ForecastCard: React.FC<IProps> = ({ dailyData }) => {
   // date
   const ms: number = dailyData.dt * 1000
   const weekDayDate: string = getFormattedDate(ms)
@@ -20,6 +21,7 @@ const ForecastCard: React.FC<IProps> = ({ dailyData, isDarkMode }) => {
   const weatherCondition: string = dailyData.weather[0].main // e.g. 'Clouds'
   const weatherDescription: string = dailyData.weather[0].description // e.g. 'overcast clouds'
   const weatherTemperature: number = dailyData.main.temp
+  const isDarkMode: boolean = useSelector((state: IRootState) => state.weather.isDarkMode) // theme
 
   const icon: JSX.Element | null = getIcon(weatherCondition, isDarkMode, s.card__icon)
 
