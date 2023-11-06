54fd28644398780b1660dc9b8de17190be106cb2
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:55:54 +0100

    added jsdoc comment in CardProgress.tsx

--- src/pages/Dashboard/Cards/CardProgress.tsx
+++ src/pages/Dashboard/Cards/CardProgress.tsx
@@ -1,10 +1,21 @@
+import { Progress, Card, Typography } from 'antd' // antd components
 import TitleCard from '../../../components/common/DashboardCards/TitleCard' // title <FC> for cards
 import { cardHeadStyle, cardBodyStyle } from '../../../utils/constants/dashboardCardsStyle' // styles objects
 import { ICardProgress } from '../../../types/types'
-import { Progress, Card, Typography } from 'antd' // antd components
 import s from './Cards.module.scss' // css file with styles
 
 const { Title } = Typography // title antd component
+
+/**
+ * A component that displays a card with progress information.
+ *
+ * @component
+ * @param {ICardProgress} props - Props for the CardProgress component.
+ * @param {string} props.title - The title for the card.
+ * @param {ReactNode} props.icon - The icon to display as part of the card.
+ * @param {number} props.data - The progress data to display.
+ * @returns {JSX.Element} JSX element containing the card with progress information.
+ */
 
 const CardProgress: React.FC<ICardProgress> = ({ title, icon, data }) => {
   return (
