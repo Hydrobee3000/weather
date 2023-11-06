1b7f6368eb6ba916cb9ab35a0c2e87ee14c48225
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 02:26:13 +0100

    Current page: change style. used roundToTenth func

--- src/pages/Current/Current.tsx
+++ src/pages/Current/Current.tsx
@@ -6,6 +6,7 @@ import { IDayWeatherData } from '../../types/types'
 import { currentPageIcons } from '../../utils/constants/pageIcons'
 import firstLetterUpperCase from '../../utils/firstLetterUpperCase' // function makes first letter in uppercase style
 import PageTitle from '../../components/common/PageTitle/PageTitle'
+import { roundToTenths } from '../../utils/roundToTenth'
 import s from './Current.module.scss' // styles
 
 const { Text } = Typography
@@ -13,49 +14,35 @@ const { Text } = Typography
 // current(main) page
 
 const Current: React.FC = () => {
-  const weatherDesc: string = useSelector((state: IRootState) => state.weather.dayWeatherData.weather[0].description) // get description of weather ex: 'cloudy'
-  const dayWeatherData: IDayWeatherData = useSelector((state: IRootState) => state.weather.dayWeatherData)
+  // data
+  const dayWeatherData: IDayWeatherData = useSelector((state: IRootState) => state?.weather?.dayWeatherData)
+  const weatherDesc: string = useSelector((state: IRootState) => state?.weather?.dayWeatherData?.weather[0]?.description) // get description of weather ex: 'cloudy'
   const IconComponent: React.ElementType = currentPageIcons.outlined
+  // weather
+  const temperature: number = roundToTenths(dayWeatherData?.main?.temp)
+  const temperatureFeelsLike: number = roundToTenths(dayWeatherData?.main?.feels_like)
 
   return (
     <div className={s.current_page}>
       <PageTitle icon={<IconComponent />}>Current conditions</PageTitle>
 
-      {/* weather icon */}
-      {/* <img className={s.current__image} src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt={dayWeatherData.description} /> */}
-
       <div className={s.info}>
+        {/* unit of temperature */}
         <Text className={s.info__temp}>
-          <span className={s.info__temp_value}>{Math.round(parseFloat(dayWeatherData.main.temp.toString()) * 10) / 10}</span>
+          <span className={s.info__temp_value}>{temperature}</span>
           <span className={s.info__temp_unit}>°С</span>
         </Text>
 
-        <Text className={s.info__feels_like}>Feels like: {dayWeatherData.main.feels_like.toFixed(1)} °С</Text>
+        {/* unit of feels like temperature */}
+        <Tag className={s.info__feels_like}>Feels like: {temperatureFeelsLike} °С</Tag>
 
+        {/* description of weather */}
         <Tag color='purple'>
           <Text className={s.info__description}>{firstLetterUpperCase(weatherDesc)}</Text>
         </Tag>
       </div>
-      {/* additional parameters */}
-      {/* <p className={s.parameter} >
-        Min t: {(dayWeatherData.main.temp_min).toFixed(1)} °С
-      </p>
-      <p className={s.parameter} >
-        Max t: {(dayWeatherData.main.temp_max).toFixed(1)} °С
-      </p>
-      <p className={s.parameter} >
-        Wind: {dayWeatherData.wind.speed} m/s
-      </p>
-      <p className={s.parameter} >
-        Humidity: {dayWeatherData.main.humidity}%
-      </p>
-      <p className={s.parameter} >
-        Pressure: {dayWeatherData.main.pressure}
-      </p>
-      <p className={s.date} >
-        {new Date().toLocaleString('en', { weekday: 'long' })} {new Date().getDate()}.{new Date().getMonth()}.
-        {new Date().getFullYear()}
-      </p> */}
+
+      {/* location */}
       <Text className={s.location}>
         <IoLocationOutline className={s.location__icon} />
         <span className={s.location__name}>{dayWeatherData.name}</span>
