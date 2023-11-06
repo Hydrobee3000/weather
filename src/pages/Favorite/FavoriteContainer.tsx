d0c72f994a2892cfbe37d1a253a921f8e135aae8
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:56:59 +0100

    added jsdoc comment in FavoriteContainer.tsx

--- src/pages/Favorite/FavoriteContainer.tsx
+++ src/pages/Favorite/FavoriteContainer.tsx
@@ -2,7 +2,12 @@ import { useEffect, useState } from 'react'
 import Preloader from '../../components/common/Preloader' // preloader to wait for data to load
 import Favorite from './Favorite'
 
-// fetch data for dashboard page and set in store
+/**
+ * A container component that fetches data for the Favorite page.
+ *
+ * @component
+ * @returns {JSX.Element} JSX element containing the Favorite page or a preloader while data is being fetched.
+ */
 
 const FavoriteContainer: React.FC = () => {
   const [ready, setReady] = useState(false)
