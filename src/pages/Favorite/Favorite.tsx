ea8f68333be63764c9fdc1e7722b7d00ca863bfa
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 02:00:42 +0100

    added logic to create favorite cards

--- src/pages/Favorite/Favorite.tsx
+++ src/pages/Favorite/Favorite.tsx
@@ -1,13 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { IRootState } from '../../redux/store'
+import { Space, Typography, Card, Tag } from 'antd'
 import { Divider, Select } from 'antd' // antd components
-import { setFavoritePlaces } from '../../redux/reducers/weatherReducer'
+import { fetchDayWeatherData, setFavoritePlaces } from '../../redux/reducers/weatherReducer'
 import PageTitle from '../../components/common/PageTitle/PageTitle'
 import { favoritesPageIcons } from '../../utils/constants/pageIcons'
 import { favoritePlacesKeyLs, saveToLocalStorage } from '../../utils/localStorage'
 import Preloader from '../../components/common/Preloader'
+import { useEffect, useState } from 'react'
+import { weatherAPI } from '../../api/api'
+import s from './Favorite.module.scss'
+import FavoriteCard from './FavoriteCard/FavoriteCard'
 
 const { Option } = Select // get option from select obj antd
+const { Text } = Typography
+
+interface IWeatherData {
+  [city: string]: any
+}
 
 // page of favorite places
 
@@ -15,13 +25,47 @@ const Favorite: React.FC = () => {
   const dispatch = useDispatch()
   const places: string[] = useSelector((state: IRootState) => state.weather.places) // array of places
   const favoritePlaces: string[] = useSelector((state: IRootState) => state.weather.favoritePlaces) // selected active place
+  const [activeFavoritePlaces, setActiveFavoritePlaces] = useState<string[]>(favoritePlaces)
+  const [isLoading, setIsLoading] = useState<Boolean>(true) // Добавляем состояние для отслеживания загрузки
+
   const IconComponent: React.ElementType = favoritesPageIcons.outlined
+  const [weatherData, setWeatherData] = useState<IWeatherData>({})
+
+  const favoriteCards = () => {
+    return activeFavoritePlaces.map((place) => <FavoriteCard key={place} weatherData={weatherData[place]} />)
+  }
 
   // handle change value of selected option
-  const onChangePlace = (favoritePlaces: string[]) => {
-    saveToLocalStorage(favoritePlacesKeyLs, favoritePlaces)
-    dispatch(setFavoritePlaces(favoritePlaces))
+  const onChangePlace = (selectedPlaces: string[]) => {
+    saveToLocalStorage(favoritePlacesKeyLs, selectedPlaces)
+    dispatch(setFavoritePlaces(selectedPlaces))
+    setActiveFavoritePlaces(selectedPlaces)
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responses = await Promise.all(
+          favoritePlaces.map(async (favPlace) => {
+            const response = await weatherAPI.getDayWeather(favPlace)
+            return { place: favPlace, data: response }
+          })
+        )
+
+        const updatedWeatherData = responses.reduce((acc, { place, data }) => {
+          return { ...acc, [place]: data }
+        }, {})
+
+        setWeatherData(updatedWeatherData)
+      } catch (error) {
+        console.error('Error while fetching data:', error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [favoritePlaces])
 
   if (!places) {
     return <Preloader />
@@ -49,7 +93,16 @@ const Favorite: React.FC = () => {
           </Option>
         ))}
       </Select>
+
       <Divider />
+
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <Space wrap size={60}>
+          {favoriteCards()}
+        </Space>
+      )}
     </>
   )
 }
