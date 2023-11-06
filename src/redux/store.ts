4f084149bf1ee6a7cf0b93b9f84864870c4fe25d
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:58:28 +0100

    added jsdoc comment in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,10 +5,16 @@ import weatherReducer from './reducers/weatherReducer'
 
 export type IRootState = ReturnType<typeof rootReducer>
 
+/**
+ * The root reducer that combines all application reducers.
+ */
 let rootReducer = combineReducers({
   weather: weatherReducer,
 })
 
+/**
+ * The Redux store that holds the complete state tree.
+ */
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
