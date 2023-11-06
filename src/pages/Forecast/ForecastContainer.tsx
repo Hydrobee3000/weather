de2b809e9a7521031f8120bb605a662536f4666f
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:57:33 +0100

    added jsdoc comment in ForecastContainer.tsx

--- src/pages/Forecast/ForecastContainer.tsx
+++ src/pages/Forecast/ForecastContainer.tsx
@@ -1,10 +1,15 @@
-import Forecast from './Forecast' // <FC> of forecast page
-import Preloader from '../../components/common/Preloader' // preloader to wait for data to load
-import { IForecastData } from '../../types/types'
 import { useSelector } from 'react-redux' // redux hooks
 import { IRootState } from '../../redux/store'
+import { IForecastData } from '../../types/types'
+import Forecast from './Forecast' // <FC> of forecast page
+import Preloader from '../../components/common/Preloader' // preloader to wait for data to load
 
-// fetch data for forecast page and set in store
+/**
+ * A container component for the "Forecast" page.
+ *
+ * @component
+ * @returns {JSX.Element} - Renders the "Forecast" page or a preloader based on available forecast data.
+ */
 
 const ForecastContainer: React.FC = () => {
   const forecastData: IForecastData | null = useSelector((state: IRootState) => state.weather.forecastData) // weather object
