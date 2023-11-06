de5305f6e85541cdff0bfc5e3f502bbf6d3a18a8
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:11:13 +0100

    added jsdoc comment in SwitcherTheme.tsx

--- src/components/common/SwitcherTheme.tsx
+++ src/components/common/SwitcherTheme.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { Switch } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { MdLightMode, MdDarkMode } from 'react-icons/md'
-import { Switch } from 'antd'
 import { IRootState } from '../../redux/store'
 import { setIsDarkMode } from '../../redux/reducers/weatherReducer'
 
-// theme mode switch
+/**
+ * Component for toggling between light and dark theme modes.
+ *
+ * @component
+ * @returns {JSX.Element} JSX element containing the theme mode switch.
+ */
 
 const SwitcherTheme: React.FC = () => {
   const dispatch = useDispatch()
   const isDarkMode: boolean = useSelector((state: IRootState) => state.weather.isDarkMode)
 
-  const toggleThemeMode = () => {
+  const toggleThemeMode = (): void => {
     dispatch(setIsDarkMode(!isDarkMode))
   }
 
