785f55a48c9dd177fb959089990e993272c429e0
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:11:37 +0100

    added jsdoc comment in SubtitleCard.tsx

--- src/components/common/DashboardCards/SubtitleCard.tsx
+++ src/components/common/DashboardCards/SubtitleCard.tsx
@@ -1,8 +1,14 @@
-// title of parameters in cards into dashboard page
-
 interface IProps {
   children: string | null
 }
+
+/**
+ * A component for displaying parameter titles in cards on a dashboard page.
+ *
+ * @component
+ * @param {string | null} props.children - The title to be displayed. If null, no title will be shown.
+ * @returns {JSX.Element} JSX element containing the parameter title with styling.
+ */
 
 const SubtitleCard: React.FC<IProps> = ({ children }) => {
   return <span style={{ color: '#783fdbe2' }}>{children}</span>
