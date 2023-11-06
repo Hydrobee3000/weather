98ffac92cb2ebeb533cff0eddeb8eaf6490d890d
Алексей Заикин <hydrombee@gmail.com>
2023-11-06 11:11:25 +0100

    added jsdoc comment in StatisticSuffix.tsx

--- src/components/common/DashboardCards/StatisticSuffix.tsx
+++ src/components/common/DashboardCards/StatisticSuffix.tsx
@@ -1,9 +1,16 @@
-// suffix of value in statistic cards into dashboard page
-
 interface IProps {
   children: string
   small?: boolean
 }
+
+/**
+ * A component for displaying a suffix in statistic cards on a dashboard page.
+ *
+ * @component
+ * @param {string} props.children - The suffix value to be displayed.
+ * @param {boolean} [props.small] - Optional. If true, the suffix will be displayed with reduced opacity.
+ * @returns {JSX.Element} JSX element containing the suffix value with optional styling.
+ */
 
 const StatisticSuffix: React.FC<IProps> = ({ children, small = false }) => {
   return <span style={small ? { opacity: '0.5' } : { fontSize: '0.6em', opacity: '0.5' }}>{children}</span>
